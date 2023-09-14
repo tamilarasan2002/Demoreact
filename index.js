09320721a8cf5f654d1c5a62a6c38600eafa5dde
tamilarasan2002 <abidm26791@gmail.com>
2023-09-14 19:58:42 +0530

    function added

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const num = [1,2,3,4,5,6,7,8,9,10]
 const even = (num) =>{
     console.log(num.filter((element,index)=>{
-        return num%2==0;
+        return element%2==0;
     }));
 }
 even(num)
+
+const odd = (num) =>{
+    console.log(num.filter((element,index)=>{
+        return element%2!=0;
+    }));
+}
+add(num)
